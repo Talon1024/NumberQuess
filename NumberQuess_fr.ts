1f550d075239b046044b9661f6975e5d339bfe9c
Kevin Caccamo <kevin@ciinet.org>
2016-10-13 04:24:09 -0400

    Initial work on French translation.
    Also, set the title of the main window in mainwindow.cpp.

--- NumberQuess_fr.ts
+++ NumberQuess_fr.ts
@@ -10,12 +10,12 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="21"/>
-        <source>Welcome to the number guessing game!</source>
+        <source>Bievenue &aacute; la jour de ...Welcome to the number guessing game!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="34"/>
-        <source>I&apos;m thinking of a number between %1 and %2. Can you guess what it is?</source>
+        <source>Je pense I&apos;m thinking of a number between %1 and %2. Can you guess what it is?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -40,7 +40,7 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="98"/>
-        <source>&amp;Game</source>
+        <source>&amp;Jour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -50,7 +50,63 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="113"/>
-        <source>Q&amp;uit</source>
+        <source>Q&amp;uitter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>guesses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>guess</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>Fin.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="69"/>
+        <source>Le jouer est fin! Press reset to play again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="64"/>
+        <source>Too high! Try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="66"/>
+        <source>Too low! Try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <source>Congratulations! You successfully guessed the number in %1 %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>tries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>try</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <source>Invalid input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <source>You must enter a valid integer.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle(tr("Number Guessing Game"));
     g = Game();
     RE_NUM = QRegularExpression("^\\d+$");
 
